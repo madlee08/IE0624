44b9fa72a71c7676f779039cdfbd87f617312eeb
Javier Solera <solerajavier96@gmail.com>
2023-09-06 11:07:50 -0600

    se completa los estados y se hace esquematico del diseno

--- lab_02/semaforo.c
+++ lab_02/semaforo.c
@@ -4,7 +4,27 @@ void main() {
     estados estado = paso_vehiculo;
 
     switch (estado) {
-        case estado: {insertar codigo} break;
+        case paso_vehiculo: 
+            if (boton){
+                if (diez_segundos){
+                    estado = parpadear_vehiculo;
+                } else estado = esperar;
+            } break;
+        
+        case esperar:
+            if (tiempo_restante){
+                estado = parpadear_vehiculo;
+            } break;
+
+        case parpadear_vehiculo:
+            if (tres_segundos){
+                estado = detener_vehiculos;
+            }
+
+        case detener_vehiculos:
+            if(un_segundo){
+                estado = paso_peaton;
+            }
         
         case paso_peaton:
             if (diez_segundos) {

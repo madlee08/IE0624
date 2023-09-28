c207cc32773cf853b3bf0c125b27f9705c2fff7c
Javier Solera <solerajavier96@gmail.com>
2023-09-27 19:11:35 -0600

    Avance del lab 3

---
+++ voltimetro/voltimetro.ino
@@ -0,0 +1,110 @@
+/*
+ * PCD8544 - Interface with Philips PCD8544 (or compatible) LCDs.
+ *
+ * Copyright (c) 2010 Carlos Rodrigues <cefrodrigues@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*
+ * To use this sketch, connect the eight pins from your LCD like thus:
+ *
+ * Pin 1 -> +3.3V (rightmost, when facing the display head-on)
+ * Pin 2 -> Arduino digital pin 3
+ * Pin 3 -> Arduino digital pin 4
+ * Pin 4 -> Arduino digital pin 5
+ * Pin 5 -> Arduino digital pin 7
+ * Pin 6 -> Ground
+ * Pin 7 -> 10uF capacitor -> Ground
+ * Pin 8 -> Arduino digital pin 6
+ *
+ * Since these LCDs are +3.3V devices, you have to add extra components to
+ * connect it to the digital pins of the Arduino (not necessary if you are
+ * using a 3.3V variant of the Arduino, such as Sparkfun's Arduino Pro).
+ */
+
+
+#include <PCD8544.h>
+
+
+// A custom glyph (a smiley)...
+static const byte glyph[] = { B00010000, B00110100, B00110000, B00110100, B00010000 };
+
+
+static PCD8544 lcd;
+
+
+void setup() {
+  // PCD8544-compatible displays may have a different resolution...
+  lcd.begin(84, 48);
+
+  // Add the smiley to position "0" of the ASCII table...
+  lcd.createChar(0, glyph);
+}
+
+
+void loop() {
+  // Just to show the program is alive...
+  int valor_adc[3];
+  valor_adc[0] = analogRead(A0);
+  valor_adc[1] = analogRead(A1);
+  valor_adc[2] = analogRead(A2);
+  valor_adc[3] = analogRead(A3);
+  //int valor_adc = analogRead(A0);
+  float volt1 = 0;
+  float volt2 = 0;
+  float volt3 = 0;
+  float volt4 = 0;
+
+  // Write a piece of text on the first line...
+  lcd.setCursor(0, 0);
+  lcd.print("Voltaje 1: ");
+  volt1 = (valor_adc[0] / 1023.0) * 5.0;
+  lcd.print(volt1);
+
+
+  // Write a piece of text on the first line...
+  lcd.setCursor(0, 1);
+  lcd.print("Voltaje 2: ");
+  volt2 = (valor_adc[1] / 1023.0) * 5.0;
+  lcd.print(volt2);
+
+
+  // Write a piece of text on the first line...
+  lcd.setCursor(0, 2);
+  lcd.print("Voltaje 3: ");
+  volt3 = (valor_adc[2] / 1023.0) * 5.0;
+  lcd.print(volt3);
+
+
+  // Write a piece of text on the first line...
+  lcd.setCursor(0, 3);
+  lcd.print("Voltaje 4: ");
+  volt4 = (valor_adc[3] / 1023.0) * 5.0;
+  lcd.print(volt4);
+
+  // Use a potentiometer to set the LCD contrast...
+  // short level = map(analogRead(A0), 0, 1023, 0, 127);
+  // lcd.setContrast(level);
+
+  delay(200);
+}
+
+
+/* EOF - HelloWorld.ino */

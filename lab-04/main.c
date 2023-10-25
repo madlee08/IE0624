03763760553b926ee3cc9338e5fd7cbadd7fca76
Mike Mai <mike.mai@ucr.ac.cr>
2023-10-24 20:56:53 -0600

    limpiar main
    Nota: Javier y yo estamos reunidos
    trabajando en una sola computadora.

--- lab-04/main.c
+++ lab-04/main.c
@@ -83,6 +83,45 @@ static void usart_setup(void)
 	usart_enable(USART_CONSOLE);
 }
 
+static void spi_setup(void) {
+	/* Enable the GPIO ports whose pins we are using */
+	rcc_periph_clock_enable(RCC_GPIOF | RCC_GPIOC);
+
+	gpio_mode_setup(GPIOF, GPIO_MODE_AF, GPIO_PUPD_PULLDOWN,
+			GPIO7 | GPIO8 | GPIO9);
+	gpio_set_af(GPIOF, GPIO_AF5, GPIO7 | GPIO8 | GPIO9);
+	gpio_set_output_options(GPIOF, GPIO_OTYPE_PP, GPIO_OSPEED_25MHZ,
+				GPIO7 | GPIO9);
+
+	/* Chip select line */
+	gpio_set(GPIOC, GPIO1);
+	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
+
+	rcc_periph_clock_enable(RCC_SPI5);
+
+	uint32_t cr_tmp = SPI_CR1_BAUDRATE_FPCLK_DIV_8 |
+		 SPI_CR1_MSTR |
+		 SPI_CR1_SPE |
+		 SPI_CR1_CPHA |
+		 SPI_CR1_CPOL_CLK_TO_1_WHEN_IDLE;
+
+	
+	SPI_CR2(SPI5) |= SPI_CR2_SSOE;
+	SPI_CR1(SPI5) = cr_tmp;
+
+	/*
+	 * These parameters are sort of random, clearly I need
+	 * set something. Based on the app note I reset the 'baseline'
+	 * values after 100 samples. But don't see a lot of change
+	 * when I move the board around. Z doesn't move at all but the
+	 * temperature reading is correct and the ID code returned is
+	 * as expected so the SPI code at least is working.
+	 */
+	write_reg(0x20, 0xcf);  /* Normal mode */
+	write_reg(0x21, 0x07);  /* standard filters */
+	write_reg(0x23, 0xb0);  /* 250 dps */
+}
+
 static void adc_setup(void)
 {
 	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0);
@@ -113,54 +152,19 @@ int main(void)
 	int16_t vecs[3];
 	int16_t baseline[3];
 	int tmp;
-	uint32_t cr_tmp;
 
 	clock_setup();
 	usart_setup();
 	adc_setup();
+	spi_setup();
 
 	/* green led for ticking */
 	gpio_mode_setup(LED_DISCO_GREEN_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
 			LED_DISCO_GREEN_PIN);
 
-	gpio_mode_setup(GPIOF, GPIO_MODE_AF, GPIO_PUPD_PULLDOWN,
-			GPIO7 | GPIO8 | GPIO9);
-	gpio_set_af(GPIOF, GPIO_AF5, GPIO7 | GPIO8 | GPIO9);
-	gpio_set_output_options(GPIOF, GPIO_OTYPE_PP, GPIO_OSPEED_25MHZ,
-				GPIO7 | GPIO9);
-
-	/* Chip select line */
-	gpio_set(GPIOC, GPIO1);
-	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
-
-	rcc_periph_clock_enable(RCC_SPI5);
-
-	cr_tmp = SPI_CR1_BAUDRATE_FPCLK_DIV_8 |
-		 SPI_CR1_MSTR |
-		 SPI_CR1_SPE |
-		 SPI_CR1_CPHA |
-		 SPI_CR1_CPOL_CLK_TO_1_WHEN_IDLE;
-
-	
-	SPI_CR2(SPI5) |= SPI_CR2_SSOE;
-	SPI_CR1(SPI5) = cr_tmp;
-	
 	baseline[0] = 0;
 	baseline[1] = 0;
 	baseline[2] = 0;
-
-	/*
-	 * These parameters are sort of random, clearly I need
-	 * set something. Based on the app note I reset the 'baseline'
-	 * values after 100 samples. But don't see a lot of change
-	 * when I move the board around. Z doesn't move at all but the
-	 * temperature reading is correct and the ID code returned is
-	 * as expected so the SPI code at least is working.
-	 */
-	write_reg(0x20, 0xcf);  /* Normal mode */
-	write_reg(0x21, 0x07);  /* standard filters */
-	write_reg(0x23, 0xb0);  /* 250 dps */
-
 
 	int16_t abel;
 

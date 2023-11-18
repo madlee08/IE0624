fb3792d98559aca706c86db09889bb0dd4d7efde
Mike Mai <mike.mai@ucr.ac.cr>
2023-11-17 18:38:24 -0600

    conectar el script con thingsboard

--- src/datos.py
+++ src/datos.py
@@ -8,56 +8,53 @@ import serial
 # https://pyserial.readthedocs.io/en/latest/pyserial.html
 
 
-# BROKER="iot.eie.ucr.ac.cr"
-# PORT = 1883
-# KEEPALIVE = 10
-# TOPIC='v1/devices/me/telemetry'
-# TOKEN = "2xxl57r9opxaqbe1ejpu"
-# DEVICE = '/dev/ttyACM0'
-# BAUDRATE = 115200
+BROKER="iot.eie.ucr.ac.cr"
+PORT = 1883
+KEEPALIVE = 10
+TOPIC='v1/devices/me/telemetry'
+TOKEN = "2xxl57r9opxaqbe1ejpu"
+DEVICE = '/dev/ttyACM0'
+BAUDRATE = 115200
 
-# def on_connect(client, userdata, flags, rc) :
-#     if (rc==0) :
-#         print(f"Conexión exitosa a {BROKER}.")
-#     else :
-#         print(f"Conexión fallida, Código de referencia: {rc}")
-
-
-# def on_disconnect(client, userdata, rc): 
-#    print(f"Se ha desconectado de {BROKER} exitosamente.") 
+def on_connect(client, userdata, flags, rc) :
+    if (rc==0) :
+        print(f"Conexión exitosa a {BROKER}.")
+    else :
+        print(f"Conexión fallida, Código de referencia: {rc}")
 
 
-# puerto = serial.Serial(DEVICE, BAUDRATE)
+def on_disconnect(client, userdata, rc): 
+   print(f"Se ha desconectado de {BROKER} exitosamente.") 
 
-# client = mqtt.Client()
-# client.on_connect = on_connect
-# client.on_disconnect = on_disconnect
 
-# client.username_pw_set(TOKEN)
-# client.connect(BROKER, PORT, KEEPALIVE)
+puerto = serial.Serial(DEVICE, BAUDRATE)
 
-# client.loop()
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+
+client.username_pw_set(TOKEN)
+client.connect(BROKER, PORT, KEEPALIVE)
+
+client.loop()
 
 try:
     while True:
-        # datos = puerto.readline().decode('utf-8', 'ignore')
-        
-        # if datos and datos.startswith('Bateria'):
-        #     print(datos, end='')
-        #     client.publish(TOPIC, '{' + datos.replace('\t', ',').removesuffix(',\r\n') + '}')
         inp = input()
-        
-        if 'lumus' in inp:
+
+        if 'lumos' in inp:
             s = '{'
+            data = inp.split(":")
             s += inp
             s += ','
             for i in range(4):
                 s += input()
                 s += ','
-            s = '}'
+            s = s[0:-2]
+            s += '}'
             print(s)
+            client.publish(TOPIC, s)
 
 except KeyboardInterrupt:
-    # puerto.close()
-    # client.disconnect()
-    pass
+    puerto.close()
+    client.disconnect()
